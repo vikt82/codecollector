0f9510a0f9cf8a6c8e9dd295406de004712c6734
Victor V <vikmobile82@gmail.com>
2019-01-30 01:05:35 +0300

    gulp_middle: css ready

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,26 @@
 "use strict";
 
 var gulp = require('gulp');
+var sass = require('gulp-sass');
 var postcss = require('gulp-postcss');
 var sourcemaps = require('gulp-sourcemaps');
 var autoprefixer = require('autoprefixer');
 var cssnano = require('cssnano');
+var mqpacker = require("css-mqpacker");
+// var cssnext = require("postcss-cssnext");
+var rucksack = require('rucksack-css');
+var lost = require('lost');
+var pxtorem = require('postcss-pxtorem');
+var pr = require('postcss-pr');
+var postcssNormalize = require('postcss-normalize');
+var zindex = require('postcss-zindex');
+
+sass.compiler = require('node-sass');
 
 var paths = {
   styles: {
-    src: 'src/scss/style.css',
-    watch: ['src/scss/**/*.css'],
+    src: ['src/scss/**/*.scss', 'src/scss/**/*.sass'],
+    watch: ['src/scss/**/*.scss', 'src/scss/**/*.sass'],
     dest: 'dist/assets/css'
   }
   // scripts: {
@@ -21,11 +32,33 @@ var paths = {
 gulp.task('styles', function() {
   var plugins = [
       autoprefixer({browsers: "last 5 versions"}),
-      cssnano()
+      postcssNormalize({
+        browserslist: "last 5 versions",
+        allowDuplicates: false,
+        browsers: 'last 5 versions',
+        forceImport: true
+      }),
+      pxtorem({
+        rootValue: 16,
+        unitPrecision: 5,
+        propList: ['font', 'font-size', 'line-height', 'letter-spacing'],
+        selectorBlackList: [],
+        replace: true,
+        mediaQuery: false,
+        minPixelValue: 0
+      }),
+      pr(),
+      rucksack({fallbacks: true}),
+      lost(),
+      // cssnext(),
+      mqpacker(),
+      zindex(),
+      // cssnano()
     ];
 
   return gulp.src(paths.styles.src)
     .pipe( sourcemaps.init() )
+    .pipe(sass({outputStyle: 'expanded'}))
     .pipe(postcss( plugins ))
     .pipe( sourcemaps.write('.') )
     .pipe(gulp.dest(paths.styles.dest));
